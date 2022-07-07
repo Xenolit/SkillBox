276bd40a8c2ad7f51b01a1c0c368938e45626dcb
Xenolit <xenolit.games@gmail.com>
2022-07-07 22:57:39 +0300

    Created folder Lesson_1
    2 types of code with output "Hello, World!!!"

---
+++ Lesson_1/HelloWorld2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld
+{
+   
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, ");
+            Console.Write("world");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
